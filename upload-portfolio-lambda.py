0c5621c05975b095819694b84a3c657419eb0c41
eiab30p <eduardo.eddy.verde94@gmail.com>
2020-07-11 20:14:50 -0400

    adding lambda script

---
+++ upload-portfolio-lambda.py
@@ -0,0 +1,19 @@
+import io
+import zipfile
+import boto3
+from botocore.client import Config
+import mimetypes
+
+s3 = boto3.resource('s3', config=Config(signature_version='s3v4'))
+
+portfolio_bucket = s3.Bucket('eduardoverde.com')
+build_bucket = s3.Bucket(r'portfolio-build-eduardoverde.com')
+
+portfolio_zip = io.BytesIO()
+build_bucket.download_fileobj(r'eduardoverdeportfoliobuild.zip', portfolio_zip)
+
+with zipfile.ZipFile(portfolio_zip) as myzip:
+    for nm in myzip.namelist():
+        obj = myzip.open(nm)
+        portfolio_bucket.upload_fileobj(obj, nm, ExtraArgs={'ContentType': mimetypes.guess_type(nm)[0]})
+        portfolio_bucket.Object(nm).Acl().put(ACL='public-read')
