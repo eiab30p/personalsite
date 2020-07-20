667c4913384537a85af0bb97b39d204ac63ebec6
eiab30p <eduardo.eddy.verde94@gmail.com>
2020-07-20 18:17:22 -0400

    added keywords to meta

--- src/App.js
+++ src/App.js
@@ -18,6 +18,10 @@ export default class App extends Component {
             content="Eduardo 'Eddy' Verde's Personal Site. Learn about who Eduardo Verde is and his Software Engineering skills."
           />
           <meta name="author" content="Eduardo Verde" />
+          <meta
+            name="keywords"
+            content="Eddy Verde, Verde, Eduardo 'Eddy' Verde, Eduardo Verde, Software, Software Engineer, Websites, Freelance, Fullstack, Full-stack, developer, computers, programming"
+          ></meta>
           <title>Eduardo "Eddy" Verde | Software Engineer</title>
           <link rel="canonical" href="https://eduardoverde.com/about" />
           <link rel="canonical" href="https://eduardoverde.com/projects" />
